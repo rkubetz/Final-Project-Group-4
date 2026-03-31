8e301a8a52f940d8cdcbbe55fa5f33ba88c1871f
Ryan Kubetz <rkubetz@charlotte.edu>
2026-03-30 20:39:05 -0400

    feat: fixed config password problem

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "Elsabra123" # make sure you use YOUR password for MySQL, that's how I got mine to work.
     app_host = "localhost"
     app_port = 8000
